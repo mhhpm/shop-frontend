f5bc68437ca0a26e25cd81984fb6cb56db0a3c6f
anhhoengdeptroai <drawk0129@gmail.com>
2022-01-04 16:23:37 +0700

    finish About page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Layout from '@utils/components/Layout'
 import { useSession, signOut } from 'next-auth/react'
 import { Col, Container, Row } from 'react-bootstrap'
 const ImgSrc = 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'
+const ImgSrc2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo2ivkG_JkakIfI1GQL1BfZlQvOQlk5Tr1bA&usqp=CAU'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -55,6 +57,34 @@ export default function Home() {
           </Row>
         </Container>
       </section>
+
+      <section className="hero-banner">
+      <div className="container">
+        <div className="row no-gutters align-items-center pt-60px">
+          <div className="col-5 d-none d-sm-block">
+            <div className="hero-banner__img">
+
+              <Image
+              className="card-img-top"
+              loader={() => ImgSrc2}
+              src={'image'}
+              alt="..."
+              width={450}
+              height={300}
+            />
+            </div>
+          </div>
+          <div className="col-sm-7 col-lg-6 offset-lg-1 pl-4 pl-md-5 pl-lg-0">
+            <div className="hero-banner__content">
+              <h4>Shop is fun</h4>
+              <h1>Browse Our Premium Product</h1>
+              <p>Us which over of signs divide dominion deep fill bring they're meat beho upon own earth without morning over third. Their male dry. They are great appear whose land fly grass.</p>
+              <a className="button button-hero" href="#">Browse Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     </Layout>
   )
 }
